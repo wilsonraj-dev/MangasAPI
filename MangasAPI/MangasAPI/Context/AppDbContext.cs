8b6eb585b683057a91561906d20f23c7b85f40b3
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-01 22:08:38 -0300

    Criação da classe de contexto da aplicação

---
+++ MangasAPI/MangasAPI/Context/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using MangasAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangasAPI.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Manga> Mangas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            builder.Entity<Categoria>().HasKey(x => x.Id);
+            builder.Entity<Categoria>().HasMany(x => x.Mangas)
+                                       .WithOne(x => x.Categoria)
+                                       .HasForeignKey(x => x.CategoriaId);
+
+            builder.Entity<Manga>().HasKey(x => x.Id);
+            builder.Entity<Manga>().Property(x => x.Titulo).HasMaxLength(100).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Descricao).HasMaxLength(200).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Autor).HasMaxLength(100).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Editora).HasMaxLength(100).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Formato).HasMaxLength(100).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Cor).HasMaxLength(50).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Origem).HasMaxLength(100).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Imagem).HasMaxLength(350).IsRequired();
+            builder.Entity<Manga>().Property(x => x.Preco).HasPrecision(10, 2).IsRequired();
+
+            foreach (var relacoes in builder.Model.GetEntityTypes().SelectMany(x => x.GetForeignKeys()))
+            {
+                relacoes.DeleteBehavior = DeleteBehavior.ClientSetNull;
+            }
+        }
+    }
+}
