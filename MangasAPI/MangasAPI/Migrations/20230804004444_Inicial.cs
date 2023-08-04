d7db6b2aba89811d25eee0792dcea594c5d6f81d
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-03 21:49:07 -0300

    Aplicação da migration Inicial

---
+++ MangasAPI/MangasAPI/Migrations/20230804004444_Inicial.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MangasAPI.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Mangas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Editora = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Paginas = table.Column<int>(type: "int", nullable: false),
+                    Publicacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Formato = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Origem = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(350)", maxLength: 350, nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Estoque = table.Column<int>(type: "int", nullable: false),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Mangas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Mangas_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Mangas_CategoriaId",
+                table: "Mangas",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Mangas");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
