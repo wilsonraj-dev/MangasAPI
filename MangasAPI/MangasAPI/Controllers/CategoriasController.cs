0420ba16d47a3867688715b2668500288eac24d5
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-03 21:43:57 -0300

    Criação das controller de categoria e mangá

---
+++ MangasAPI/MangasAPI/Controllers/CategoriasController.cs
@@ -0,0 +1,103 @@
+﻿using AutoMapper;
+using MangasAPI.DTOs;
+using MangasAPI.Entities;
+using MangasAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangasAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ICategoriaRepository _categoriaRepository;
+        private readonly IMapper _mapper;
+        public CategoriasController(ICategoriaRepository categoryRepository,
+            IMapper mapper)
+        {
+            _categoriaRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<IEnumerable<CategoriaDTO>>> Get()
+        {
+            var categorias = await _categoriaRepository.GetAllAsync();
+            if (categorias is null)
+            {
+                return NotFound("Categorias não existem");
+            }
+
+            var categoriasDto = _mapper.Map<IEnumerable<CategoriaDTO>>(categorias);
+            return Ok(categoriasDto);
+        }
+
+        [HttpGet("{id:int}", Name = "GetCategoria")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<CategoriaDTO>> Get(int id)
+        {
+            var categoria = await _categoriaRepository.GetByIdAsync(id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada");
+            }
+
+            var categoriaDto = _mapper.Map<CategoriaDTO>(categoria);
+            return Ok(categoriaDto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Post([FromBody] CategoriaDTO categoriaDto)
+        {
+            if (categoriaDto == null)
+            {
+                return BadRequest("Dados inválidos");
+            }
+
+            var categoria = _mapper.Map<Categoria>(categoriaDto);
+            await _categoriaRepository.AddAsync(categoria);
+
+            return new CreatedAtRouteResult("GetCategoria", new { id = categoriaDto.Id }, categoriaDto);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Put(int id, [FromBody] CategoriaDTO categoriaDto)
+        {
+            if (id != categoriaDto.Id)
+            {
+                return BadRequest();
+            }
+            if (categoriaDto == null)
+            {
+                return BadRequest();
+            }
+
+            var categoria = _mapper.Map<Categoria>(categoriaDto);
+            await _categoriaRepository.UpdateAsync(categoria);
+
+            return Ok(categoriaDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var categoria = await _categoriaRepository.GetByIdAsync(id);
+            if (categoria == null)
+            {
+                return NotFound("Categoria não encontrada");
+            }
+
+            await _categoriaRepository.RemoveAsync(id);
+            return Ok(categoria);
+        }
+    }
+}
---
+++ MangasAPI/MangasAPI/Controllers/MangasController.cs
@@ -0,0 +1,144 @@
+﻿using AutoMapper;
+using MangasAPI.DTOs;
+using MangasAPI.Entities;
+using MangasAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangasAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MangasController : ControllerBase
+    {
+        private readonly IMangaRepository _mangaRepository;
+        private readonly IMapper _mapper;
+
+        public MangasController(IMangaRepository mangaRepository,
+            IMapper mapper)
+        {
+            _mangaRepository = mangaRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            var mangas = await _mangaRepository.GetAllAsync();
+            return Ok(_mapper.Map<IEnumerable<MangaDTO>>(mangas));
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var manga = await _mangaRepository.GetByIdAsync(id);
+
+            if (manga is null)
+            {
+                return NotFound($"Manga com {id} não encontrado");
+            }
+
+            return Ok(_mapper.Map<MangaDTO>(manga));
+        }
+
+        [HttpGet]
+        [Route("get-mangas-by-category/{categoryId:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetMangasByCategory(int categoryId)
+        {
+            var mangas = await _mangaRepository.GetMangasPorCategoriaAsync(categoryId);
+
+            if (!mangas.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<MangaDTO>>(mangas));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Add(MangaDTO mangaDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var manga = _mapper.Map<Manga>(mangaDto);
+            await _mangaRepository.AddAsync(manga);
+
+            return Ok(_mapper.Map<MangaDTO>(manga));
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, MangaDTO mangaDto)
+        {
+            if (id != mangaDto.Id)
+            {
+                return BadRequest();
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _mangaRepository.UpdateAsync(_mapper.Map<Manga>(mangaDto));
+            return Ok(mangaDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var manga = await _mangaRepository.GetByIdAsync(id);
+            if (manga is null)
+            {
+                return NotFound();
+            }
+
+            await _mangaRepository.RemoveAsync(manga.Id);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("search/{mangaTitulo}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<MangaDTO>>> Search(string mangaTitulo)
+        {
+            var mangas = await _mangaRepository.SearchAsync(m => m.Titulo.Contains(mangaTitulo));
+
+            if (mangas is null)
+            {
+                return NotFound("Nenhum mangá foi encontrado");
+            }
+
+            return Ok(_mapper.Map<IEnumerable<MangaDTO>>(mangas));
+        }
+
+        [HttpGet]
+        [Route("search-manga-with-category/{criterio}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<List<MangaCategoriaDTO>>> SearchMangaWithCategory(string criterio)
+        {
+            var mangas = _mapper.Map<List<Manga>>(await _mangaRepository.LocalizarMangasComCategoriaAsync(criterio));
+
+            if (!mangas.Any())
+            {
+                return NotFound("Nenhum mangá foi encontrado");
+            }
+
+            return Ok(_mapper.Map<IEnumerable<MangaCategoriaDTO>>(mangas));
+        }
+    }
+}
