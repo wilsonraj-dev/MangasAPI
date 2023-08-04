e61ed06536e94ab005a603482af80e62aace9be7
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-08-03 21:32:33 -0300

    Definição dos serviços e conexão com banco de dados na classe Program

--- MangasAPI/MangasAPI/Program.cs
+++ MangasAPI/MangasAPI/Program.cs
@@ -1,11 +1,27 @@
+using MangasAPI.Context;
+using MangasAPI.Mappings;
+using MangasAPI.Repositories;
+using MangasAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+                .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(op => op.UseSqlServer(connection,
+                                            x => x.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddScoped<IMangaRepository, MangaRepository>();
+builder.Services.AddAutoMapper(typeof(DomainToDTOProfile));
 
 var app = builder.Build();
 
